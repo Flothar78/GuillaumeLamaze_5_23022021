e4bcebd08ace835fc063a82b375534f546bf6554
Guillaume Lamaze <lamazeguillaume@hotmail.fr>
2021-06-16 12:00:02 +0200

    fetchtest index2 modifiés

--- fetchtest.js
+++ fetchtest.js
@@ -1,24 +1,22 @@
-fetch("http://localhost:3000/api/furniture/")
-  .then(function (response) {
-    if (response.ok) {
-      return response.json();
-    }
-  })
-  .then(function (value) {
-    console.log(value);
-  })
-  .catch(function (err) {});
+const displayName = document.querySelector("#Name");
+const displayPrice = document.getElementById("Price");
+const displayImage = document.getElementById("Image");
+const displayDescription = document.querySelector("#Description");
 
-var furniture = [
-  "CrossTable",
-  "Coffee Table",
-  "Dining Table",
-  "Bench",
-  "Vintage Chair",
-];
-var text = "";
-var i;
-for (i = 0; i < furniture.length; i++) {
-  text += funiture[i] + "<br>";
-}
-document.getElementById("demo").innerHTML = text;
+const promise01 = fetch("http://localhost:3000/api/furniture/");
+promise01
+  .then((getData) => {
+    return getData.json();
+  })
+  .then((consoleData) => {
+    console.log(consoleData[3].description);
+    let txtName = consoleData[3].name;
+    let txtPrice = consoleData[3].price;
+    let txtImage = consoleData[3].imageUrl;
+    let txtDescription = consoleData[3].description;
+
+    displayName.innerHTML = txtName;
+    displayPrice.innerHTML = txtPrice;
+    displayImage.innerHTML = txtImage;
+    displayDescription.innerHTML = txtDescription;
+  });
