94c78531ffe98f4c94368f5c7d745ac0b959143e
Guillaume Lamaze <lamazeguillaume@hotmail.fr>
2021-06-17 18:21:55 +0200

    fetchtest modifié

--- fetchtest.js
+++ fetchtest.js
@@ -1,23 +1,21 @@
+var url = new URL(window.location.href);
+const id = url.searchParams.get("id");
+
 const displayName = document.getElementById("Name");
 const displayPrice = document.getElementById("Price");
 const displayImage = document.getElementById("Image");
 const displayDescription = document.getElementById("Description");
 
-const promise01 = fetch("http://localhost:3000/api/furniture/");
+const promise01 = fetch("http://localhost:3000/api/furniture/" + id);
 promise01
   .then((getData) => {
     return getData.json();
   })
   .then((consoleData) => {
-    for (let i = 0; i < consoleData.length; i++) {
-      console.log(consoleData[i]);
-    }
-
-    let txtName = consoleData[1].name;
-    let txtPrice = consoleData[1].price;
-    let txtImage = consoleData[1].imageUrl;
-    let txtDescription = consoleData[1].description;
-
+    let txtName = consoleData.name;
+    let txtPrice = consoleData.price;
+    let txtImage = consoleData.imageUrl;
+    let txtDescription = consoleData.description;
     displayName.innerHTML = txtName;
     displayPrice.innerHTML = txtPrice;
     displayDescription.innerHTML = txtDescription;
