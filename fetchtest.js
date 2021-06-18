b4734bcb510a83df79410d2191e7ca21d4a2002c
Guillaume Lamaze <lamazeguillaume@hotmail.fr>
2021-06-18 15:27:05 +0200

    commit de trucs

--- fetchtest.js
+++ fetchtest.js
@@ -5,19 +5,23 @@ const displayName = document.getElementById("Name");
 const displayPrice = document.getElementById("Price");
 const displayImage = document.getElementById("Image");
 const displayDescription = document.getElementById("Description");
+const displayVarnish = document.getElementById("Varnish");
 
 const promise01 = fetch("http://localhost:3000/api/furniture/" + id);
 promise01
   .then((getData) => {
     return getData.json();
   })
-  .then((consoleData) => {
-    let txtName = consoleData.name;
-    let txtPrice = consoleData.price;
-    let txtImage = consoleData.imageUrl;
-    let txtDescription = consoleData.description;
+  .then((apiFurniture) => {
+    let txtName = apiFurniture.name;
+    let txtPrice = apiFurniture.price;
+    let txtImage = apiFurniture.imageUrl;
+    let txtDescription = apiFurniture.description;
+    let txtVarnish = apiFurniture.varnish;
+
     displayName.innerHTML = txtName;
     displayPrice.innerHTML = txtPrice;
     displayDescription.innerHTML = txtDescription;
     displayImage.src = txtImage;
+    displayVarnish.innerHTML = txtVarnish;
   });
