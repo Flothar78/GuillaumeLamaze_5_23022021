3a16810e404bb22784b225d45ae618891f1b9f09
Guillaume Lamaze <lamazeguillaume@hotmail.fr>
2021-06-26 22:03:17 +0200

    second commit suite changement materiel

---
+++ index.js
@@ -0,0 +1,42 @@
+const api = fetch("http://localhost:3000/api/furniture/");
+
+api
+  .then((getData) => {
+    return getData.json();
+  })
+  .then((displayData) => {
+    for (var i = 0; i < displayData.length; i++) {
+      var newDiv = document.createElement("div");
+      newDiv.className = "items";
+      var section = document.getElementById("section");
+      section.append(newDiv);
+      newDiv.innerHTML =
+        '<img class="card-img-top"' +
+        "src=" +
+        displayData[i].imageUrl +
+        " />" +
+        '<div class="card-body p-4">' +
+        '<div class="text-center">' +
+        "<div>" +
+        '<h5 class="fw-bolder">' +
+        displayData[i].name +
+        "</h5>" +
+        "</div>" +
+        displayData[i].price / 100 +
+        "€" +
+        "</div>" +
+        "</div>" +
+        '<div class="card-footer p-4 pt-0 border-top-0 bg-transparent">' +
+        '<div class="text-center">' +
+        '<button class="ToPageProducts" type="button">' +
+        '<a href="products.html?id=' +
+        displayData[i]._id +
+        '"> Voir le produit</a>' +
+        "</button>" +
+        "</div>" +
+        "</div>";
+    }
+  })
+  .catch((error) => {
+    console.log("Une erreur est survenue");
+  });
