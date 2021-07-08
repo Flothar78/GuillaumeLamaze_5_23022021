361d2da8d9e95f8607cdbea917aa6ca0cf697066
Guillaume Lamaze <lamazeguillaume@hotmail.fr>
2021-07-08 18:02:31 +0200

    Panier OK sauf calculs

--- cart.js
+++ cart.js
@@ -1,47 +1,33 @@
 
 
-window.addEventListener('storage', () => {
-    console.log(localStorage);
-var newDiv = document.createElement("div");
-var appendDiv = document.getElementById("appendDiv");
-appendDiv.append(newDiv);
-newDiv.innerHTML =
-'<div class="row main align-items-center">' +
-                    '<div class="col">' +
-                     '<div class="choiceName"></div>' +
-                   '</div>' +
-           '<div class="col">' +
-            '<div class="choicePrice"></div>' +
-             ' </div>' +
-              '<div class="col">' +
-               '<div class="choiceQuantity"></div>' +
-                '</div>' +
-                 '</div>' +
-               '</div>'});
+
 
 
 for (let i = 0 ; i < localStorage.length ; i++) {
+
+   var newTr = document.createElement("tr");
+   var appendTr = document.getElementById("appendTr");
+   appendTr.append(newTr);
+   newTr.innerHTML =
+   '<td class="choiceName"></td>' +
+   '<td class="choicePrice"></td>' + 
+   '<td class="choiceQuantity"></td>' +
+   '</tr>';    
     
     let nameProduct = JSON.parse(localStorage.getItem(localStorage.key(i))).name;
-    let nameCart = document.querySelector('.choiceName');
+    let nameCart = document.querySelectorAll('.choiceName')[i];
     nameCart.innerHTML += nameProduct;  
 
     let priceProduct = JSON.parse(localStorage.getItem(localStorage.key(i))).price/100 +'€';
-    let priceCart = document.querySelector('.choicePrice');
+    let priceCart = document.querySelectorAll('.choicePrice')[i];
     priceCart.innerHTML += priceProduct;
 
     let quantityProduct = JSON.parse(localStorage.getItem(localStorage.key(i))).quantity;
-    let quantityCart = document.querySelector('.choiceQuantity');
+    let quantityCart = document.querySelectorAll('.choiceQuantity')[i];
     quantityCart.innerHTML += quantityProduct;
 
+    
 }
-
-
- 
-
-
-
-
 
 
 
